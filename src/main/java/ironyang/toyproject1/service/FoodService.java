5fdf57001d3b20c1445d06cce8b2a32b1cbdc8f2
fDevJc <fdevjc@gmail.com>
2022-06-02 16:23:57 +0900

    [feat] FoodService addFood(), findFood() 구현 및 테스트코드 작성

---
+++ src/main/java/ironyang/toyproject1/service/FoodService.java
@@ -0,0 +1,24 @@
+package ironyang.toyproject1.service;
+
+import ironyang.toyproject1.domain.Food;
+import ironyang.toyproject1.repository.FoodRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class FoodService {
+    private final FoodRepository foodRepository;
+
+    @Transactional
+    public Long addFood(Food food) {
+        return foodRepository.save(food).getId();
+    }
+
+    public Food findFood(Long foodId) {
+        return foodRepository.findById(foodId)
+                .orElseThrow(() -> new RuntimeException());
+    }
+}
---
+++ src/test/java/ironyang/toyproject1/service/FoodServiceTest.java
@@ -0,0 +1,47 @@
+package ironyang.toyproject1.service;
+
+import ironyang.toyproject1.domain.Food;
+import ironyang.toyproject1.repository.FoodRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class FoodServiceTest {
+    @Mock
+    FoodRepository foodRepository;
+    @InjectMocks
+    FoodService foodService;
+
+    @Test
+    void addFoodAndFindFood() {
+        //given
+        Food resultFood = new Food();
+        resultFood.setId(1L);
+        resultFood.setName("떡볶이");
+        resultFood.setPrice(15_000);
+        given(foodRepository.save(any(Food.class))).willReturn(resultFood);
+        given(foodRepository.findById(1L)).willReturn(Optional.of(resultFood));
+
+        //when
+        Food paramFood = new Food();
+        paramFood.setName("떡볶이");
+        paramFood.setPrice(15_000);
+        Long savedFoodId = foodService.addFood(paramFood);
+        Food foundFood = foodService.findFood(savedFoodId);
+
+        //then
+        assertThat(foundFood.getName()).isEqualTo("떡볶이");
+        assertThat(foundFood.getPrice()).isEqualTo(15_000);
+    }
+}
