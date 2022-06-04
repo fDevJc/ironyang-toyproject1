62c9597212478fad26c06a35a7ce8ab70993e135
fDevJc <fdevjc@gmail.com>
2022-06-04 22:11:41 +0900

    [test]UserControllerTest join() 추가

---
+++ src/test/java/ironyang/toyproject1/web/UserControllerTest.java
@@ -0,0 +1,34 @@
+package ironyang.toyproject1.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class UserControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void join() throws Exception {
+        //given
+        UserRequestDto userRequestDto = new UserRequestDto();
+        //when & then
+        mockMvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andDo(print());
+
+    }
+}
